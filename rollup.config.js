47ce8e2288aa1d2e14136df030458e1811134236
jaxx2104 <jaxx2104@gmail.com>
2019-01-01 22:20:07 +0900

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,19 @@
-module.exports = {
-  input: "src/main.js",
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  external: ['path'],
+  input: 'src/main.js',
   output: {
-    file: "dist/bundle.js",
-    format: "cjs"
-  }
-};
+    file: 'dist/bundle.js',
+    format: 'umd'
+  },
+  plugins: [
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    babel()
+  ]
+}
