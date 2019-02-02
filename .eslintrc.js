0f2ed079a28d858d7fa9441688d8353b6be1f9e1
jaxx2104 <jaxx2104@gmail.com>
2019-02-02 16:58:52 +0900

    fix ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,10 @@ module.exports = {
   root: true,
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
-    '@typescript-eslint/indent': 'off',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/indent': 'off',
     '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     'no-console': 'off',
     'prettier/prettier': 'error'
   }
